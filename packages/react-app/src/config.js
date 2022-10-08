a63fde9901ee847e7650e34e13e8c663fae1c369
G. Ellegeest <gwen.ellegeest@outlook.com>
2022-10-09 00:32:02 +0200

    Cryptonaise Swap

---
+++ packages/react-app/src/config.js
@@ -0,0 +1,14 @@
+import { Goerli } from "@usedapp/core";
+
+export const ROUTER_ADDRESS = "0x8EeB119acCdbD1713446e7f357594675c175b18b"; 
+
+export const DAPP_CONFIG = {
+  readOnlyChainId: Goerli.chainId,
+  readOnlyUrls: {
+    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/yL_l_NX73-FMyL5yv-d5FMpPNwBnhBsP",
+  },
+};
+
+// 0x8EeB119acCdbD1713446e7f357594675c175b18b
+
+// 0xB44aBa5d2053201cb918687D1856a34a61FA43bE
